c85ae740bbddd5d5c7042913b6d69007f90588fe
Abhishekranjan8051 <abhishekranjan8051@gmail.com>
2023-05-15 23:15:39 +0530

    project list

---
+++ src/components/ProjectList.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import Calculator from "../assests/image/calculator.png";
+import FindImage from "../assests/image/findimage.png";
+
+const Project = () => {
+	return (
+		<>
+			{" "}
+			<h1>Project List</h1>{" "}
+			<div>
+				
+				<Card
+					style={{
+						padding: "15px",
+						display: "flex",
+						flexDirection: "row",
+						flexWrap: "wrap",
+						gap: "10px",
+						width: "fit-content",
+						justifyContent: "center",
+						margin: "auto",
+					}}
+				>
+					<Card style={{ width: "fit-content", padding: "10px" }}>
+						{" "}
+						<img width="180" height="180" alt="test" src={Calculator} />{" "}
+						<span>Calculator App</span>{" "}
+						<a href="https://reactcalculatorsapp.netlify.app/">
+							{" "}
+							Demo
+						</a>{" "}
+					</Card>{" "}
+					<Card style={{ width: "fit-content", padding: "10px" }}>
+						<img width="180" height="180" alt="test" src={FindImage} />{" "}
+						<span>Calculator App</span>{" "}
+						<a href="https://reactcalculatorsapp.netlify.app/">
+							{" "}
+							Demo
+						</a>{" "}
+					</Card>
+					<Card style={{ width: "fit-content", padding: "10px" }}>
+						<img width="180" height="180" alt="test" src={FindImage} />{" "}
+						<span>Calculator App</span>{" "}
+						<a href="https://reactcalculatorsapp.netlify.app/">
+							{" "}
+							Demo
+						</a>{" "}
+					</Card>
+					<Card style={{ width: "fit-content", padding: "10px" }}>
+						<img width="180" height="180" alt="test" src={FindImage} />{" "}
+						<span>Calculator App</span>{" "}
+						<a href="https://reactcalculatorsapp.netlify.app/">
+							{" "}
+							Demo
+						</a>{" "}
+					</Card>{" "}
+				</Card>{" "}
+			</div>{" "}
+		</>
+	);
+};
+
+export default Project;
